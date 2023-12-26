6d5c74410c2b2cb3f0bfe2df04b943e591ec3fec
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2023-12-26 09:36:44 +0800

    Update NavBar.jsx 🛠️
     - Adding the `elements` for the Navbar. Preparing for styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,19 @@
 export default function NavBar() {
   return (
     <>
-      <h1>NavBar</h1>
+      <nav>
+        <h1>snap</h1>
+        <ul>
+          <li>Features</li>
+          <li>Company</li>
+          <li>Careers</li>
+          <li>About</li>
+        </ul>
+        <div>
+          <button>Login</button>
+          <button>Register</button>
+        </div>
+      </nav>
     </>
   );
 }
