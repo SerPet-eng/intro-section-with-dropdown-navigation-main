7877f6ab7c3beb55e4daadcbd9044f51875911bf
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2023-12-26 09:31:52 +0800

    Setting Up `Components` 🗄️🛠️
    Things that has been added:
     - Body.jsx
     - NavBar.jsx
      > DropdownMenu Folder
        * Features.jsx
        * Company.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import NavBar from './components/NavBar/NavBar';
+import Body from './components/Body';
+
 export default function App() {
   return (
     <>
       <h1>Sane Test</h1>
+      <NavBar />
+      <Body />
     </>
   );
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,7 @@
+export default function Body() {
+  return (
+    <>
+      <h1>Body</h1>
+    </>
+  );
+}
