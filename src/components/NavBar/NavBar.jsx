23892f40043ebf29d260da1f8a19bcf7e0611c6f
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2023-12-27 11:30:57 +0800

    Updating `classname` to Navbar.jsx🛠️

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,15 @@
 export default function NavBar() {
   return (
     <>
-      <nav className="navbar">
-        <h1 className="navbar__title">snap</h1>
+      <nav className="nav">
+        <h1 className="nav__title">snap</h1>
         <ul>
           <li>Features</li>
           <li>Company</li>
           <li>Careers</li>
           <li>About</li>
         </ul>
-        <div className="button__section">
+        <div className="nav__btn-section">
           <button className="button">Login</button>
           <button className="button">Register</button>
         </div>
