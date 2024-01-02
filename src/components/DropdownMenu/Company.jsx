857c30cabb6f0db3af2800312b341e0b783ad10a
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2024-01-02 21:12:23 +0800

    Adding style to the `Features`✨🛠️
     - Adding margins to the features and company dropdown menu
    
     - Also adding a gap and margin-bottom to the menu and menu__btn.

--- src/components/DropdownMenu/Company.jsx
+++ src/components/DropdownMenu/Company.jsx
@@ -1,11 +1,13 @@
 export default function Company() {
   return (
     <>
-      <ul>
-        <li>History</li>
-        <li>Our Team</li>
-        <li>Blog</li>
-      </ul>
+      <div className="company">
+        <ul>
+          <li>History</li>
+          <li>Our Team</li>
+          <li>Blog</li>
+        </ul>
+      </div>
     </>
   );
 }
