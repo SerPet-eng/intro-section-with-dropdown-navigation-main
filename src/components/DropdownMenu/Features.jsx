f168e8816c15d94e316c32ca78ad01263e6e2e60
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2024-01-02 17:42:31 +0800

    Update Features.jsx 🛠️
     - Adding necessary elements and images to the Features dropdown menu

--- src/components/DropdownMenu/Features.jsx
+++ src/components/DropdownMenu/Features.jsx
@@ -1,7 +1,29 @@
+import Todo from '../../../images/icon-todo.svg';
+import Calendar from '../../../images/icon-calendar.svg';
+import Reminders from '../../../images/icon-reminders.svg';
+import Planning from '../../../images/icon-planning.svg';
+
 export default function Features() {
   return (
     <>
-      <h1>Features</h1>
+      <ul>
+        <li>
+          <img src={Todo} alt="Todo Icon" />
+          <span>Todo</span>
+        </li>
+        <li>
+          <img src={Calendar} alt="Calendar Icon" />
+          <span>Calendar</span>
+        </li>
+        <li>
+          <img src={Reminders} alt="Reminders Icon" />
+          <span>Reminders</span>
+        </li>
+        <li>
+          <img src={Planning} alt="Planning Icon" />
+          <span>Planning</span>
+        </li>
+      </ul>
     </>
   );
 }
